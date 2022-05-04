7247ba4f7237c439648f1b00a6c74cad489371ff
Hayel <ivanforwork@yandex.ru>
2022-05-04 19:08:11 +0300

    Application class added.

---
+++ src/main/java/by/teachmeskills/spring/veterinary_clinic/VeterinaryClinicApplication.java
@@ -0,0 +1,11 @@
+package by.teachmeskills.spring.veterinary_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryClinicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinaryClinicApplication.class, args);
+    }
+}
